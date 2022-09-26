a10b463e262764dbce00ce6a5e9dc1145d4691e9
Dor Shtaif <dorshtaif@gmail.com>
2022-09-26 20:55:13 +0300

    little syntactic sugaring

--- src/qyu-error.ts
+++ src/qyu-error.ts
@@ -5,9 +5,7 @@ class QyuError<CodeType extends string> extends Error {
     super(message);
     this.code = code;
     // Maintains proper stack trace for where our error was thrown (only available on V8) (reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#es6_customerror_class)
-    if ((Error as any).captureStackTrace) {
-      (Error as any).captureStackTrace(this, new.target);
-    }
+    (Error as any).captureStackTrace?.(this, new.target);
   }
 }
 
